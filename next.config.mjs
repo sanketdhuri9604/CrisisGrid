cde00258d541d682a54147e19740c860eebec268
monik diwani <monikdiwani@gmail.com>
2026-04-04 10:55:33 +0530

    chore: remove Next.js dev indicator from UI

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  devIndicators: {
+    appIsrStatus: false,
+    buildActivity: false,
+  },
 };
 
 export default nextConfig;
